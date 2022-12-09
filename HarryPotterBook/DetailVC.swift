18983f48533f03d4580439978d155bcba960ad0a
zynpbyrkdmrl <zeynepbayrak863@gmail.com>
2022-12-09 13:57:50 +0300

    first commit

---
+++ HarryPotterBook/DetailVC.swift
@@ -0,0 +1,33 @@
+//
+//  DetailVC.swift
+//  HarryPotterBook
+//
+//  Created by Zeynep Bayrak Demirel on 9.12.2022.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    @IBOutlet weak var imageview: UIImageView!
+    @IBOutlet weak var namelabel: UILabel!
+    @IBOutlet weak var agelabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- HarryPotterBook/ViewController.swift
+++ HarryPotterBook/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableview.delegate = self
+        tableview.dataSource = self
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "zeynep"
+        return cell
+    }
 
 }
 
